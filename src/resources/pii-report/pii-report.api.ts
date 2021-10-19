6d51fd47358e3114277be0ff76a0267767839db5
Nick Rogers <rogersnick@live.com>
2021-10-19 16:15:56 -0600

    update readme add tests

--- src/resources/pii-report/pii-report.api.ts
+++ src/resources/pii-report/pii-report.api.ts
@@ -3,27 +3,52 @@ import globalAxios, { AxiosInstance, AxiosPromise, AxiosRequestConfig } from 'ax
 import { Configuration } from '../../configuration';
 import { getAxiosRequestArgs } from '../../lib/get-axios-request-args';
 import { RequestArgs } from '../../tonic';
-import { CancelPiiReportParams, JobScanModel, MostRecentActiveOrCompletedParams, PiiReportAPI, PiiReportAPIRequestArgsCreator, PiiReportParams, PiiReportRun, StartPiiReportParams } from './pii-report.api.types';
+import {
+  CancelPiiReportParams,
+  JobScanModel,
+  MostRecentActiveOrCompletedParams,
+  PiiReportAPI,
+  PiiReportAPIRequestArgsCreator,
+  PiiReportParams,
+  PiiReportRun,
+  StartPiiReportParams,
+} from './pii-report.api.types';
 
 const piiReportApiRequestArgsCreator = (configuration: Configuration): PiiReportAPIRequestArgsCreator => {
   return {
-    getGetMostRecentActiveOrCompletedStatusRequestArgs: async (params: MostRecentActiveOrCompletedParams,options?: AxiosRequestConfig): Promise<RequestArgs> => getAxiosRequestArgs('GET', `/api/PiiReport/most_recent_active_or_completed_status`, params, configuration, options),
-    getPiiReportRequestArgs: async (params: PiiReportParams, options?: AxiosRequestConfig): Promise<RequestArgs> =>  getAxiosRequestArgs('GET', `/api/PiiReport`, params, configuration, options),
-    getStartPiiReportRequestArgs: async (params: StartPiiReportParams, options?: AxiosRequestConfig): Promise<RequestArgs> =>  getAxiosRequestArgs('POST', `/api/PiiReport/start`, params, configuration, options),
-    getCancelPiiReportRequestArgs: async (params: CancelPiiReportParams, options?: AxiosRequestConfig): Promise<RequestArgs> => getAxiosRequestArgs('POST', `/api/PiiReport/cancel`, params, configuration, options),
+    getGetMostRecentActiveOrCompletedStatusRequestArgs: async (
+      params: MostRecentActiveOrCompletedParams,
+      options?: AxiosRequestConfig
+    ): Promise<RequestArgs> =>
+      getAxiosRequestArgs(
+        'GET',
+        `/api/PiiReport/most_recent_active_or_completed_status`,
+        params,
+        configuration,
+        options
+      ),
+    getPiiReportRequestArgs: async (params: PiiReportParams, options?: AxiosRequestConfig): Promise<RequestArgs> =>
+      getAxiosRequestArgs('GET', `/api/PiiReport`, params, configuration, options),
+    getStartPiiReportRequestArgs: async (
+      params: StartPiiReportParams,
+      options?: AxiosRequestConfig
+    ): Promise<RequestArgs> => getAxiosRequestArgs('POST', `/api/PiiReport/start`, params, configuration, options),
+    getCancelPiiReportRequestArgs: async (
+      params: CancelPiiReportParams,
+      options?: AxiosRequestConfig
+    ): Promise<RequestArgs> => getAxiosRequestArgs('POST', `/api/PiiReport/cancel`, params, configuration, options),
   };
 };
 
-const piiReportApi = (configuration:Configuration): Partial<PiiReportAPI> => {
+const piiReportApi = (configuration: Configuration): PiiReportAPI => {
   return {
     async getMostRecentActiveOrCompletedPiiReport(
       params: MostRecentActiveOrCompletedParams,
       options?: AxiosRequestConfig
     ): Promise<(axios: AxiosInstance, basePath: string) => AxiosPromise<JobScanModel>> {
-      const requestAxiosArgs = await piiReportApiRequestArgsCreator(configuration).getGetMostRecentActiveOrCompletedStatusRequestArgs(
-        params,
-        options
-      );
+      const requestAxiosArgs = await piiReportApiRequestArgsCreator(
+        configuration
+      ).getGetMostRecentActiveOrCompletedStatusRequestArgs(params, options);
 
       return (axios: AxiosInstance = globalAxios, basePath: string): AxiosPromise<JobScanModel> => {
         const axiosRequestArgs = { ...requestAxiosArgs.options, url: basePath + requestAxiosArgs.url };
@@ -77,8 +102,6 @@ const piiReportApi = (configuration:Configuration): Partial<PiiReportAPI> => {
       };
     },
   };
-}
+};
 
-
-
-export { piiReportApi, piiReportApiRequestArgsCreator }
+export { piiReportApi, piiReportApiRequestArgsCreator };
